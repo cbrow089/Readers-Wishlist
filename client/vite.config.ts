2d8cee9bc3cec224d517bd5e8f5cba3afad3ea16
cbrow089 <cbrow089@yahoo.com>
2024-12-06 17:34:51 -0500

    added another change to the vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,11 +8,14 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
+      '/graphal': {
         target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true
       }
     }
+  },
+  build: {
+    outDir: 'dist', // Change this to your desired output directory if needed
   }
 })
